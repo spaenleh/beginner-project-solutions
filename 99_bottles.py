71e422ca16af7fd8df7ca8e1c098125d6ad5cc9c
spaenleh <basile.spaenlehauer@epfl.ch>
2018-10-20 17:45:47 +0200

    Changed the total to a variable for more readability.

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,4 +1,5 @@
-for i in range(99, 0, -1):
+total = 99
+for i in range(total, 0, -1):
     if i == 1:
         print("1 bottle of beer on the wall, 1 bottle of beer.")
         print("Take it down and pass it around, no more bottles of beer on the wall.")
@@ -10,4 +11,4 @@ for i in range(99, 0, -1):
         print("Take it down and pass it around, {0} bottle of beer on the wall.".format(i - 1))
 
 print("No more bottles of beer on the wall, no more bottles of beer.")
-print("So go to the store and buy some more. 99 bottles of beer on the wall.")
+print("So go to the store and buy some more. {0} bottles of beer on the wall.".format(total))
